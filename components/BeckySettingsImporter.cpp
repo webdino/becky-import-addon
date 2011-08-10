c85e5e8360ecbcf8b73926ecf5d2c7bba3c6308f
Hiroyuki Ikezoe <hiikezoe@mozilla-japan.org>
2011-08-11 08:26:07 +0900

    start implementation of settings import.

--- components/BeckySettingsImporter.cpp
+++ components/BeckySettingsImporter.cpp
@@ -40,6 +40,11 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif /* HAVE_CONFIG_H */
+
+#include <nsMsgBaseCID.h>
+#include <nsIMsgAccountManager.h>
+#include <nsServiceManagerUtils.h>
+
 #include "BeckySettingsImporter.h"
 
 NS_IMPL_ISUPPORTS1(BeckySettingsImporter, nsIImportSettings)
@@ -68,11 +73,61 @@ BeckySettingsImporter::SetLocation(nsIFile *aLocation)
   return NS_ERROR_NOT_IMPLEMENTED;
 }
 
+nsresult
+GetIncomingServer(const nsString &aUserName,
+                  const nsString &aServerName,
+                  nsIMsgIncomingServer **aServer)
+{
+  nsresult rv;
+  nsCOMPtr<nsIMsgAccountManager> accountManager = do_GetService(NS_MSGACCOUNTMANAGER_CONTRACTID, &rv);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsCOMPtr<nsIMsgIncomingServer> incomingServer;
+  rv = accountManager->FindServer(NS_ConvertUTF16toUTF8(aUserName),
+                                  NS_ConvertUTF16toUTF8(aServerName),
+                                  NS_LITERAL_CSTRING("pop3"),
+                                  getter_AddRefs(incomingServer));
+
+  if (NS_FAILED(rv) || !incomingServer) {
+    rv = accountManager->CreateIncomingServer(NS_ConvertUTF16toUTF8(aUserName),
+                                              NS_ConvertUTF16toUTF8(aServerName),
+                                              NS_LITERAL_CSTRING("pop3"),
+                                              getter_AddRefs(incomingServer));
+  }
+  NS_IF_ADDREF(*aServer = incomingServer);
+
+  return NS_OK;
+}
+
 NS_IMETHODIMP
 BeckySettingsImporter::Import(nsIMsgAccount **aLocalMailAccount NS_OUTPARAM,
                               PRBool *_retval NS_OUTPARAM)
 {
-  return NS_ERROR_NOT_IMPLEMENTED;
+  nsresult rv;
+  nsAutoString userName, serverName;
+
+  nsCOMPtr<nsIMsgIncomingServer> incomingServer;
+  rv = GetIncomingServer(userName, serverName, getter_AddRefs(incomingServer));
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsCOMPtr<nsIMsgAccountManager> accountManager =
+    do_GetService(NS_MSGACCOUNTMANAGER_CONTRACTID, &rv);
+
+  nsCOMPtr<nsIMsgIncomingServer> localFoldersServer;
+  accountManager->GetLocalFoldersServer(getter_AddRefs(localFoldersServer));
+
+  nsCOMPtr<nsIMsgAccount> localFoldersAccount;
+  accountManager->FindAccountForServer(localFoldersServer,
+                                       getter_AddRefs(localFoldersAccount));
+
+  nsCOMPtr<nsIMsgAccount> account;
+  rv = accountManager->CreateAccount(getter_AddRefs(account));
+  rv = account->SetIncomingServer(incomingServer);
+
+  if (aLocalMailAccount)
+    account.forget(aLocalMailAccount);
+  *_retval = PR_TRUE;
+  return NS_OK;
 }
 
 nsresult
--- test/settings.test.js
+++ test/settings.test.js
@@ -1,10 +1,21 @@
 var description = 'Setting component tests'
 var gSettings;
+var actualAccount;
 
 function setUp() {
+  actuaAccount = null;
 }
 
 function tearDown() {
+  if (actualAccount) {
+    var accountManager = Cc["@mozilla.org/messenger/account-manager;1"].getService(Ci.nsIMsgAccountManager);
+    accountManager.removeAccount(actualAccount);
+  }
+}
+
+assert.equalAccount = function(expected, actual) {
+  assert.isInstanceOf(Ci.nsIMsgAccount, actual);
+  assert.equals(expected.toString(), actual.toString());
 }
 
 testCreate.description = "create instance test";
@@ -12,5 +23,19 @@ testCreate.priority = 'must';
 function testCreate() {
   gSettings = Cc["@mozilla-japan.org/import/becky/settings;1"].getService(Ci.nsIImportSettings);
   assert.isDefined(gSettings);
+}
+
+testImport.description = "import test";
+testImport.priority = 'must';
+function testImport() {
+  testCreate();
+  var container = {};
+  assert.isTrue(gSettings.Import(container));
+  assert.isDefined(container.value);
+
+  actualAccount = container.value;
+  var expected = {
+  };
+  assert.equalAccount(expected, actualAccount);
 }
 
