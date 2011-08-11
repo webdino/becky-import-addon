1d7c8e0065c76d3df1a80d7721d605a06afb6343
Hiroyuki Ikezoe <hiikezoe@mozilla-japan.org>
2011-08-12 08:29:52 +0900

    some tests for mail importer.

--- test/mail.test.js
+++ test/mail.test.js
@@ -14,3 +14,33 @@ function testCreate() {
   assert.isDefined(gMail);
 }
 
+testGetDefaultLocation.description = "GetDefaultLocation test";
+testGetDefaultLocation.priority = 'must';
+function testGetDefaultLocation() {
+  testCreate();
+  var userVerify = {};
+  var found = {};
+  var location = {};
+  gMail.GetDefaultLocation(location, found, userVerify);
+  assert.isTrue(userVerify.value);
+  assert.isTrue(userVerify.found);
+}
+
+testFindMailboxes.description = "FindMailboxes test";
+testFindMailboxes.priority = 'must';
+function testFindMailboxes() {
+  testCreate();
+  var mailboxes = gMail.FindMailboxes(utils.normalizeToFile(utils.baseURL));
+  assert.isDefined(mailboxes);
+}
+
+testTranslateFolderName.description = "TranslateFolderName test";
+testTranslateFolderName.priority = 'must';
+function testTranslateFolderName() {
+  testCreate();
+  assert.equals("Sent", gMail.TranslateFolderName("sent"));
+  assert.equals("Trash", gMail.TranslateFolderName("trash"));
+  assert.equals("Inbox", gMail.TranslateFolderName("inbox"));
+  assert.equals("Unsent Messages", gMail.TranslateFolderName("unsent"));
+}
+
