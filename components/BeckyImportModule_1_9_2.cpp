d986fa2ce82d1b6c7c8cdf4db3aa001b9f0ec8db
Hiroyuki Ikezoe <hiikezoe@mozilla-japan.org>
2011-08-17 07:48:05 +0900

    Need a cast for MSVC.
    Now we can build with Visual Studio 2008!

--- components/BeckyImportModule_1_9_2.cpp
+++ components/BeckyImportModule_1_9_2.cpp
@@ -89,7 +89,7 @@ BeckyRegister(nsIComponentManager *aCompManager,
 static const nsModuleComponentInfo components[] = {
   { "Becky! Import Component", MJ_BECKYIMPORT_CID,
     "@mozilla-japan.org/import/becky;1", BeckyImportConstructor,
-    BeckyRegister, nsnull },
+    (NSRegisterSelfProcPtr)BeckyRegister, nsnull },
   { "Becky! Import Settings Component", MJ_BECKYIMPORT_SETTINGS_CID,
     MJ_BECKYIMPORT_SETTINGS_CONTRACT_ID,
     BeckySettingsImporterConstructor },
