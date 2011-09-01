21671a0ce95c8b8a4d24c8083b916a4057a9ea7a
Hiroyuki Ikezoe <hiikezoe@mozilla-japan.org>
2011-09-01 20:15:39 +0900

    Import address book into personal address book.

--- components/BeckyProfileMigrator.cpp
+++ components/BeckyProfileMigrator.cpp
@@ -205,6 +205,13 @@ BeckyProfileMigrator::ImportAddressBook()
                                          getter_AddRefs(mGenericImporter));
   NS_ENSURE_SUCCESS(rv, rv);
 
+  nsCOMPtr<nsISupportsCString> pabString = do_CreateInstance(NS_SUPPORTS_CSTRING_CONTRACTID, &rv);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  // we want to migrate the outlook express addressbook into our personal address book
+  pabString->SetData(nsDependentCString("moz-abmdbdirectory://abook.mab"));
+  mGenericImporter->SetData("addressDestination", pabString);
+
   PRBool imported;
   PRBool wantsProgress;
   mGenericImporter->WantsProgress(&wantsProgress);
